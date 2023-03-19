5462165c27b1405ce7d602cc661ce6bc186d6b39
vahiddarabi1373730 <db.vahid1373@gmail.com>
2023-03-19 08:18:47 -0700

    typeo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'rxjs';
+  inputText: string = '';
+  result: boolean = false;
+  randomText: string = 'man vahid darabi hastm az tehran';
+
+  onChangeInput(text: any) {
+    this.inputText = text;
+  }
+
+  compare(letter: string, inputTextt: string) {
+    if (!inputTextt) {
+      return '';
+    }
+    return inputTextt === letter ? 'is-success' : 'is-danger';
+  }
 }
