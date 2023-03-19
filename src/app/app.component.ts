63840d1bd09a1d901c2ea2fb0f55c9f5f4fb0177
vahiddarabi1373730 <db.vahid1373@gmail.com>
2023-03-19 04:38:13 -0700

    cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  posts = [
+    {
+      title: 'test 1',
+      image: 'assets/1.png',
+      username: 'test1',
+      content: 'test1',
+    },
+    {
+      title: 'test 2',
+      image: 'assets/2.png',
+      username: 'test2',
+      content: 'test2',
+    },
+    {
+      title: 'test 3',
+      image: 'assets/3.png',
+      username: 'test3',
+      content: 'test3',
+    },
+  ];
   title = 'rxjs';
 }
