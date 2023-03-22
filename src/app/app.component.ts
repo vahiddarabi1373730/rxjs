ed8fc56450f138e1b554d7915b641fd5a2471728
vahiddarabi1373730 <db.vahid1373@gmail.com>
2023-03-22 07:54:46 -0700

    game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { MathValidation } from './validation';
+import { debounceTime, delay } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'rxjs';
+export class AppComponent implements OnInit {
+  public correctNumber: number = 0;
+  public secondPerSolution: number = 0;
+  constructor(private fb: FormBuilder) {}
+
+  highlight(a, b, total) {
+    if (a + b * 0.8 < total) {
+      return true;
+    }
+    return false;
+  }
+  ngOnInit(): void {
+    const startTime = new Date();
+    this.form.controls['total'].valueChanges.subscribe((res) => {
+      const total = this.a + this.b;
+
+      if (parseInt(res) === total) {
+        this.correctNumber++;
+        this.secondPerSolution =
+          (new Date().getTime() - startTime.getTime()) /
+          this.correctNumber /
+          1000;
+        this.form.patchValue({
+          a: this.randomNumber(),
+          b: this.randomNumber(),
+          total: '',
+        });
+      }
+    });
+  }
+
+  public form: FormGroup = new FormGroup(
+    {
+      a: this.fb.control(this.randomNumber()),
+      b: this.fb.control(this.randomNumber()),
+      total: this.fb.control(''),
+    },
+    [MathValidation.additional('total', 'a', 'b')]
+  );
+
+  randomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
+
+  public get a() {
+    return this.form.controls['a'].value;
+  }
+  public get b() {
+    return this.form.controls['b'].value;
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
+import { HighLight } from './highligh.directive';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HighLight],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
